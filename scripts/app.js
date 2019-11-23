45c0f62883df4ce37c0bdb2264cb85a1f635857c
alejandro-castrillon <4lejo.castrillon@gmail.com>
2019-11-23 10:55:44 -0500

    Add functional links to redirect another pages related with operative system
    Change some menu items.
    
    Add functionality at buttons to redirect another related pages.

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+
+$('#download').click(function(){
+    window.open("http://pokoxemo.blogspot.com/2014/02/descarga-wifislax-os-varias-versiones.html","self")
+})
+
+$('#forum').click(function(){
+    window.open("https://foro.seguridadwireless.net/live-wifislax/","self")
+})
+
+$('.active').click(function(){
+    $("#section").load("history.html")
+})
