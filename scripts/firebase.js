4c9d88c94488083f800e093b7e1982bf8a9a0261
alejandro-castrillon <4lejo.castrillon@gmail.com>
2019-12-04 22:11:58 -0500

    Add to firebase database a new comment object with the generated id and a valid state, show all comments in database by console

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,12 +1,34 @@
 const db = firebase.database();
 
+$(document).ready(function () {
+	console.clear()
+	db.ref().on("value", function (snapshot) {
+
+		const objects = snapshot.val()
+		console.log(objects)
+
+		for (const key in objects) {
+			const element = objects[key]
+			console.log(key, element)
+		}
+	}, function (error) {
+		console.log("Error: " + error.code);
+	});
+})
+
 $("#comments-form").submit(function (event) {
-    const data = $(this).serializeArray()
+	event.preventDefault();
 
-    const userName = data[0][1]
-    const userComment = data[1][1]
+	var key = db.ref().push().key
+	var object = {}
+	object[key] = {
+		id: key,
+		name: $('#name-input').val(),
+		comment: $('#comment-area').val(),
+		replies: [],
+		valid: true
+	}
 
-    $(this).trigger('reset')
-
-    event.preventDefault();
+	db.ref().update(object)
+	$(this).trigger('reset')
 })
