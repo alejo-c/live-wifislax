454ada31ef153c90828a04e74ec85936d8458386
alejo-castrillon <4lejo.castrillon@gmail.com>
2019-12-06 13:40:56 -0500

    Updates

---
+++ precache-manifest.aa90c8430b0f6d5257147abe1e36a1da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569b1b0be13dbb7a8d5943dffef77bec",
+    "url": "/live-wifislax/index.html"
+  },
+  {
+    "revision": "65069d2784b0192f756a",
+    "url": "/live-wifislax/static/css/main.4f275c36.chunk.css"
+  },
+  {
+    "revision": "a118f8c33985d9b3e392",
+    "url": "/live-wifislax/static/js/2.69b7fcff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/live-wifislax/static/js/2.69b7fcff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65069d2784b0192f756a",
+    "url": "/live-wifislax/static/js/main.d4995027.chunk.js"
+  },
+  {
+    "revision": "60e1b8bd17f2624abbe6",
+    "url": "/live-wifislax/static/js/runtime-main.0fd0338b.js"
+  },
+  {
+    "revision": "7de7847e2f77c899049f2c044b988889",
+    "url": "/live-wifislax/static/media/banner.7de7847e.png"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/live-wifislax/static/media/bootstrap-logo.73844123.png"
+  }
+]);
