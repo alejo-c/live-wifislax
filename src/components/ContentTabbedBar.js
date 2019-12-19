d4cd629ec87172224bc0de273dd2f04f02ed8a7a
alejandro-castrillon <4lejo.castrillon@gmail.com>
2019-12-18 21:48:31 -0500

    Replace the mdbreact elements to DOM elements

--- src/components/ContentTabbedBar.js
+++ src/components/ContentTabbedBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router } from 'react-router-dom'
-import { MDBNavbar, MDBNavItem, MDBNavbarBrand, MDBNavbarToggler, MDBCollapse, MDBNavbarNav, MDBNavLink, MDBTabContent, MDBTabPane } from "mdbreact"
+import { MDBNavbar, MDBNavItem, MDBNavbarToggler, MDBCollapse, MDBNavbarNav, MDBNavLink, MDBTabContent, MDBTabPane } from "mdbreact"
 
 import Start from './content/Start'
 import History from './content/History'
@@ -33,51 +33,53 @@ export default class ContentTabbedBar extends Component {
 
 		return <Router>
 			<MDBNavbar color="elegant-color-dark" dark expand="md">
-				<MDBNavItem>
+				<div className='nav-item'>
 					<img className='mr-3' src={this.props.image} width='40' alt='tabbed-nav' />
-				</MDBNavItem>
+				</div>
 
-				<MDBNavbarBrand>
+				<div className='navbar-brand'>
 					<strong className="white-text">{this.props.title}</strong>
-				</MDBNavbarBrand>
+				</div>
 
 				<MDBNavbarToggler onClick={this.toggleCollapse} />
 
-				<MDBCollapse id="navbarCollaps3" isOpen={this.state.isOpen} navbar>
+				<MDBCollapse isOpen={this.state.isOpen} navbar>
 					<MDBNavbarNav left>
 						{
 							this.props.contentTabs.map((tab, i) => {
 								if (this.state.activeItem === (i + 1) + '') {
-									return <MDBNavItem key={i} active>
+									return <MDBNavItem className='active' key={i}>
 										<MDBNavLink
 											to="#"
-											active={this.state.activeItem === (i + 1) + ''}
 											onClick={this.toggle((i + 1) + '')}
-											role="tab"
-										>{tab.title}</MDBNavLink>
+										>
+											{tab.title}
+										</MDBNavLink>
 									</MDBNavItem>
 								}
 								return <MDBNavItem key={i}>
 									<MDBNavLink
 										to="#"
-										active={this.state.activeItem === (i + 1) + ''}
 										onClick={this.toggle((i + 1) + '')}
-										role="tab"
-									>{tab.title}</MDBNavLink>
+									>
+										{tab.title}
+									</MDBNavLink>
 								</MDBNavItem>
 							})
 						}
 						<MDBNavItem>
 							<MDBNavLink to="#" onClick={() => {
 								window.open(this.props.linkTab.href, '_self')
-							}}>{this.props.linkTab.title}</MDBNavLink>
+							}}>
+								{this.props.linkTab.title}
+							</MDBNavLink>
 						</MDBNavItem>
 					</MDBNavbarNav>
 				</MDBCollapse>
 			</MDBNavbar>
 
 			<div className='row text-left'>
-				<MDBTabContent className="card col-10" activeItem={this.state.activeItem}>
+				<div className="tab-content card col-10" activeItem={this.state.activeItem}>
 					<MDBTabPane tabId='1' role="tabpanel">
 						<div className="scrollbar" style={scrollbarStyle}>
 							<Start />
@@ -101,7 +103,7 @@ export default class ContentTabbedBar extends Component {
 							<Versions />
 						</div>
 					</MDBTabPane>
-				</MDBTabContent>
+				</div>
 
 				<div className='bg-dark text-white col-2'>
 					<SideLinksPage menus={menuLinks} />
