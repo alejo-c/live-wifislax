77dc21fb424fec3582aac4db7a485dc9d9e39e4c
alejandro-castrillon <4lejo.castrillon@gmail.com>
2019-12-18 22:46:02 -0500

    Change the elements spacing

--- src/components/SideLinksPage.js
+++ src/components/SideLinksPage.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react"
-import { MDBContainer, MDBCollapse, MDBListGroup, MDBListGroupItem } from "mdbreact"
+import { MDBCollapse, MDBListGroup, MDBListGroupItem } from "mdbreact"
 
 export default class SideLinksPage extends Component {
 	constructor() {
 		super()
-		this.state = {collapseID: ""}
+		this.state = { collapseID: "" }
 	}
 
 	toggleCollapse = collapseID => () => {
@@ -14,33 +14,30 @@ export default class SideLinksPage extends Component {
 	}
 
 	render() {
-		return <div>
-			{
-				this.props.menus.map((menu, i) => {
-					return <MDBListGroup key={i}>
-						<MDBContainer
-							color="primary"
-							onClick={this.toggleCollapse(i)}
-							style={{ marginBottom: "1rem" }}
-						>
-							{menu.title}
-							<i className={'ml-1 '+(this.state.collapseID === {i} ? "fa fa-angle-down" : "fa fa-angle-down")} />
-						</MDBContainer>
-						
-						<MDBCollapse id={i} isOpen={this.state.collapseID}>
-							<MDBListGroup>
-								{
-									menu.links.map((link, j) => {
-										return <MDBListGroupItem className='elegant-color' key={j}>
-											<a className='text-white' href={link.href}>{link.text}</a>
-										</MDBListGroupItem>
-									})
-								}
-							</MDBListGroup>
-						</MDBCollapse>
+		return this.props.menus.map((menu, i) => {
+			var angle = this.state.collapseID === i ? "fa fa-angle-up" : "fa fa-angle-down"
+			return <MDBListGroup key={i}>
+				<div
+					className='container'
+					onClick={this.toggleCollapse(i)}
+					style={{ marginTop: "10px" }}
+				>
+					{menu.title}
+					<i className={'ml-1 ' + angle} />
+				</div>
+
+				<MDBCollapse id={i} isOpen={this.state.collapseID}>
+					<MDBListGroup>
+						{
+							menu.links.map((link, j) => {
+								return <MDBListGroupItem className='elegant-color' key={j}>
+									<a className='text-white' href={link.href}>{link.text}</a>
+								</MDBListGroupItem>
+							})
+						}
 					</MDBListGroup>
-				})
-			}
-		</div >
+				</MDBCollapse>
+			</MDBListGroup>
+		})
 	}
 }
