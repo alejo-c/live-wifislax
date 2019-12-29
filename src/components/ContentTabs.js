03a520aa2d4ea23a6d26f8bd61760c3a4f7af173
alejandro-castrillon <4lejo.castrillon@gmail.com>
2019-12-29 18:06:50 -0500

    Add margin top

--- src/components/ContentTabs.js
+++ src/components/ContentTabs.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { MDBTabContent, MDBTabPane } from "mdbreact"
+import React, { Component } from 'react';
+import { MDBTabContent, MDBTabPane } from 'mdbreact'
 
 import Start from './content/Start'
 import History from './content/History'
@@ -10,30 +10,30 @@ import './scrollable.css'
 
 export default class ContentTab extends Component {
 	render() {
-		const scrollbarStyle = { overflowY: "scroll", height: "500px" }
+		const scrollbarStyle = { overflowY: 'scroll', height: '500px' }
 
-		return <div className='text-left'>
-			<MDBTabContent className="card" activeItem={this.props.activeTab}>
-				<MDBTabPane tabId='1' role="tabpanel">
-					<div className="scrollable" style={scrollbarStyle}>
+		return <div className='text-left mt-3'>
+			<MDBTabContent className='card' activeItem={this.props.activeTab}>
+				<MDBTabPane tabId='1' role='tabpanel'>
+					<div className='scrollable' style={scrollbarStyle}>
 						<Start />
 					</div>
 				</MDBTabPane>
 
-				<MDBTabPane tabId='2' role="tabpanel">
-					<div className="scrollable" style={scrollbarStyle}>
+				<MDBTabPane tabId='2' role='tabpanel'>
+					<div className='scrollable' style={scrollbarStyle}>
 						<History />
 					</div>
 				</MDBTabPane>
 
-				<MDBTabPane tabId='3' role="tabpanel">
-					<div className="scrollable" style={scrollbarStyle}>
+				<MDBTabPane tabId='3' role='tabpanel'>
+					<div className='scrollable' style={scrollbarStyle}>
 						<Instalation />
 					</div>
 				</MDBTabPane>
 
-				<MDBTabPane tabId='4' role="tabpanel">
-					<div className="scrollable" style={scrollbarStyle}>
+				<MDBTabPane tabId='4' role='tabpanel'>
+					<div className='scrollable' style={scrollbarStyle}>
 						<Versions />
 					</div>
 				</MDBTabPane>
