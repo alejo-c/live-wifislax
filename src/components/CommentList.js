4ff8afab5130ab1755b24ec2ad4356cd9454b096
alejandro-castrillon <4lejo.castrillon@gmail.com>
2020-01-14 00:03:43 -0500

    Send the username and onCommentError props to Comment element

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 
-import Comment from "./Comment";
+import Comment from './Comment';
 
 export default class CommentList extends Component {
 	render() {
 		return Object.keys(this.props.comments).map((key, index) => {
 			return <Comment
 				key={index}
+				username={this.props.username}
 				comment={this.props.comments[key]}
 				path={this.props.path}
 				onCommentReply={this.props.onCommentReply}
 				onCommentReport={this.props.onCommentReport}
+				onCommentError={this.props.onCommentError}
 			/>
 		})
 	}
