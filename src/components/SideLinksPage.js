3d6c321a95793443b795b6e6c1dd6ede2165e0f2
alejo-castrillon <4lejo.castrillon@gmail.com>
2020-01-20 17:56:33 -0500

    Fix the collapse action, sending to toggleCollapse method a string

--- src/components/SideLinksPage.js
+++ src/components/SideLinksPage.js
@@ -17,24 +17,36 @@ export default class SideLinksPage extends Component {
 		return <div>
 			{
 				this.props.menus.map((menu, i) => {
-					const angle = this.state.collapseID === i ? 'fa fa-angle-up' : 'fa fa-angle-down'
-					return <MDBListGroupItem className='text-white elegant-color-dark m-0 pr-0 text-left' key={i}>
-
-						<div onClick={this.toggleCollapse(i)}>
+					const angle = this.state.collapseID === i
+						? 'fa fa-angle-up'
+						: 'fa fa-angle-down'
+					return <MDBListGroupItem
+						className='text-white elegant-color-dark m-0 pr-0 text-left'
+						key={i}
+					>
+						<div onClick={this.toggleCollapse(`${i}`)}>
 							{menu.title}<i className={'ml-1 ' + angle} />
 						</div>
-
 						<MDBCollapse id={`${i}`} isOpen={this.state.collapseID}>
 							<MDBListGroup>
 								{
 									menu.links.map((link, j) => {
-										return <MDBListGroupItem className='elegant-color' key={j}>
-											<a className='text-white' href={link.href} target='_blank' rel='noopener noreferrer'>{link.text}</a>
+										return <MDBListGroupItem
+											className='elegant-color'
+											key={j}
+										>
+											<a
+												className='text-white'
+												href={link.href}
+												target='_blank'
+												rel='noopener noreferrer'
+											>{link.text}</a>
 										</MDBListGroupItem>
 									})
 								}
 							</MDBListGroup>
 						</MDBCollapse>
+
 					</MDBListGroupItem>
 				})
 			}
