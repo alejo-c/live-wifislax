640ad015969afb1f5e26f62dce5b86fa483ee588
alejandro-castrillon <4lejo.castrillon@gmail.com>
2020-01-24 19:03:00 -0500

    Receive the array content elements by paramether to make better the class functionality

--- src/components/ContentTabs.js
+++ src/components/ContentTabs.js
@@ -1,53 +1,25 @@
 import React, { Component } from 'react';
 import { MDBTabContent, MDBTabPane } from 'mdbreact'
 
-import Start from './content/Start'
-import History from './content/History'
-import Versions from './content/Versions'
-import Instalation from './content/Instalation'
-
 import './scrollable.css'
 
 export default class ContentTab extends Component {
 	render() {
-		const scrollbarStyle = { overflowY: 'scroll', height: '500px' }
-
-		return <MDBTabContent className='card mt-sm-2 mx-sm-2' activeItem={this.props.activeTab}>
-			<MDBTabPane
-				tabId='1'
-				role='tabpanel'
-				className='scrollable'
-				style={scrollbarStyle}
-			>
-				<Start />
-			</MDBTabPane>
-
-			<MDBTabPane
-				tabId='2'
-				role='tabpanel'
-				className='scrollable'
-				style={scrollbarStyle}
-			>
-				<History />
-			</MDBTabPane>
-
-			<MDBTabPane
-				tabId='3'
-				role='tabpanel'
-				className='scrollable'
-				style={scrollbarStyle}
-			>
-				<Instalation />
-			</MDBTabPane>
-
-			<MDBTabPane
-				tabId='4'
-				role='tabpanel'
-				className='scrollable'
-				style={scrollbarStyle}
-			>
-				<Versions />
-			</MDBTabPane>
+		return <MDBTabContent
+			className='card mt-sm-2 mx-sm-2'
+			activeItem={this.props.activeTab}
+		>
+			{
+				this.props.contents.map((content, i) => {
+					return <MDBTabPane
+						key={i}
+						tabId={`${i + 1}`}
+						role='tabpanel'
+						className='scrollable'
+						style={{ overflowY: 'scroll', height: '500px' }}
+					>{content}</MDBTabPane>
+				})
+			}
 		</MDBTabContent>
 	}
 }
